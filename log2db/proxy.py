ec425eedb2fb22e71703cca1010c4135d72730d3
暁东邪 <xiaodongxiexie@users.noreply.github.com>
2021-04-21 11:35:37 +0800

    增加一个动态配置feature

--- log2db/proxy.py
+++ log2db/proxy.py
@@ -10,4 +10,40 @@ class _Proxy:
 
 class Proxy(_Proxy):
     pass
-   
+
+class Config:
+    _cfg = {}
+
+    def set(self, key, value):
+        self._cfg[key] = value
+        return self
+
+    @classmethod
+    def get(cls, key):
+        return cls._cfg[key]
+
+    def __getattr__(self, item):
+        def _(*args, **kwargs):
+            if not args and not kwargs:
+                return self
+            if len(args) == 1 and not kwargs.pop("_keeplist", False):
+                self.set(item, args[0])
+            else:
+                self.set(item, args)
+            for key, value in kwargs.items():
+                if key == "_keeplist":
+                    continue
+                self.set(key, value)
+            self.__dict__ = self._cfg
+            return self
+        return _
+
+
+if __name__ == '__main__':
+
+    Config().url("localhost")
+    Config().echo(False)
+
+    print(Config.get("url"))
+    print(Config.get("echo"))
+  
